9a24420db9b41d03947ca64a449001d610c33d43
freer4an <freer4an@gmail.com>
2023-08-09 20:46:12 +0600

    test reset

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,22 +6,22 @@ import (
 	"os"
 	"testing"
 
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 const driverName = "postgres"
+const DB_SOURCE = "postgresql://ady:password@localhost:5432/simple_bank?sslmode=disable"
 
 var testStore Store
 
 func TestMain(m *testing.M) {
 	var err error
 
-	if err = godotenv.Load(); err != nil {
-		log.Fatal(err)
-	}
+	// if err = godotenv.Load(); err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	testDB, err := sql.Open(driverName, os.Getenv("DB_SOURCE"))
+	testDB, err := sql.Open(driverName, DB_SOURCE)
 	if err != nil {
 		log.Fatal("Connection to db failed:", err)
 	}
